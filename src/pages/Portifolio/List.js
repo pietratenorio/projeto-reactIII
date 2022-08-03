dd67e9ce3c8c6b41784307e1150b7f61bbd24da3
Pietra Tenório <tenoriopietra@gmail.com>
2022-08-03 16:05:12 -0300

    adicionando atividade

---
+++ src/pages/Portifolio/List.js
@@ -0,0 +1,37 @@
+const List = [
+  {   
+      id: 1,
+      nome: "Meu Primeiro Portfólio",
+      descricao: "Projeto Desenvolvido em HTML e CSS",
+      conteudo: "Feito em HTML5 e CSS3",
+      imagem: "https://media2.giphy.com/media/WlgTj1sRRxKQHbD13c/giphy.gif?cid=790b76118f264e2c70060c24218e5b85c6133cf4a0bbf095&rid=giphy.gif&ct=g",
+      link: "https://github.com/pietratenorio/On17-TodasEmTech-HTML-CSS-I"
+  },
+  {
+      id: 2,
+      nome: "M de Maravilhosa",
+      descricao: "Projeto Desenvolvido em HTML e CSS + Responsividade", 
+      conteudo: "HTML e CSS + Responsividade",
+      imagem: "https://ibb.co/y84M4MD",
+      link: "https://github.com/pietratenorio/On17-TodasEmTech-MdeMaravilhosa"
+  },
+  {    
+    id: 3,
+    nome: "To Do List",
+    descricao: "Projeto Desenvolvido em HTML, CSS E JAVASCRIPT",
+    conteudo: "HTML, CSS E JAVASCRIPT",
+    imagem: "https://ibb.co/n7Dkhd8",
+    link: "https://github.com/pietratenorio/On17-TodasEmTech-ListToDo"
+  },
+  {
+      id: 4,
+      nome: "Pesquisa Digimon",
+      descricao: "Projeto Desenvolvido em HTML, CSS E JAVASCRIPT", 
+      conteudo: "HTML, CSS E JAVASCRIPT",
+      imagem: "https://media1.giphy.com/media/YfEUYH4ys3thDaVLCn/giphy.gif",
+      link: "https://github.com/pietratenorio/On17-TodasEmTech-JavascriptV"
+  }
+
+]
+
+export default List
